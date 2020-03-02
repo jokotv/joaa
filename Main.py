ed340999f46c655780a2d8267084abdbbd50c2d1
jokotv <47844827+jokotv@users.noreply.github.com>
2020-03-02 15:26:52 +0900

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,38 @@
+import discord
+import asyncio
+import openpyxl
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("준비 완료 했습니다. 보스")
+    game = discord.Game("Madby:김조코")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("조아"):
+        file = openpyxl.load_workbook("학습데이터베이스.xlsx")
+        sheet = file.active
+        memory = message.content.split(" ")
+        for i in range(1,3000):
+            if sheet["A" + str(i)].value == memory[1]:
+                await message.channel.send(sheet["B" + str(i)].value)
+                break
+
+    if message.content.startswith("조아학습"):
+        file = openpyxl.load_workbook("학습데이터베이스.xlsx")
+        sheet = file.active
+        learn = message.content.split("/")
+        for i in range(1, 3000):
+            if sheet["A" + str(i)].value == "-" or sheet["A" + str(i)].value == learn[1]:
+                sheet["A" + str(i)].value = learn[1]
+                sheet["B" + str(i)].value = learn[2]
+                await message.channel.send("학습완료.")
+                print("학습완료.", learn[1],learn[2])
+                break
+        file.save("학습데이터베이스.xlsx")
+
+client.run("NjgzNjQwNDg4ODY3MTM1NjQx.Xlugfg.0z8Q9vkLRQGcDCSYbG-7LVX0tbE")
